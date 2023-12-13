8c587cb0e08ad02f2099c7f27fff7351e752be24
Cold-iu <colescharly@yahoo.com>
2023-12-12 19:35:35 -0500

    secret_route

--- Maze/Victory.gd
+++ Maze/Victory.gd
@@ -6,6 +6,7 @@ func _ready():
 
 func _on_win_area_body_entered(body):
 	if len(Global.inventory) > 0:
-		get_tree().quit()
+		get_tree().change_scene_to_file("res://UI/secret_ending.tscn")
 	else:
 		print("You need a key") # Replace with function body.
+
